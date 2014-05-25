29efc5a60b112ce6ed61fb5bbdcabfd6ed957e60
Vadym Tyemirov <temirov@gmail.com>
2014-05-25 12:10:35 -0700

    Iteration count bug in benchmark fixed

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -1,32 +1,28 @@
 require 'benchmark'
 require_relative '../prime_multi'
 
-Benchmark.bm do |r| 
+Benchmark.bmbm do |r| 
   r.report "Find prime numbers using native Prime class" do
-    100000.times do
-      i = +1 
+    400.times do |i|
       PrimeMulti.list i, method: :native
     end
   end
 
   r.report "Find prime numbers using non-optimized while break cycle" do
-    100000.times do
-      i = +1 
+    400.times do |i|
       PrimeMulti.list i, method: :non_optimized
     end
   end
 
   r.report "Find prime numbers using optimized while break cycle" do
-    100000.times do
-      i = +1 
+    400.times do |i|
       PrimeMulti.list i, method: :optimized
     end
   end
 
   r.report "Find prime numbers using Fiber iterations" do
-    100000.times do
-      i = +1 
-      PrimeMulti.list i, method: :native
+    400.times do |i|
+      PrimeMulti.list i, method: :fiber
     end
   end
 end
