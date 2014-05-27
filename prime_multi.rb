eec7d2e387964b6fc7315a4b4079d68a6a422578
Vadym Tyemirov <temirov@gmail.com>
2014-05-27 00:30:48 -0700

    explanatory notes added

--- prime_multi.rb
+++ prime_multi.rb
@@ -3,6 +3,7 @@ require_relative 'table'
 
 class PrimeMulti
   class << self
+    # NOTE list_non_optimized has 2 nested iterations amounting to O(n^2) complexity
     def list_non_optimized total, candidate: 2
       raise ArgumentError unless total.is_a? Integer
       primes = []
@@ -32,6 +33,9 @@ class PrimeMulti
         list_fibers total, candidate: start_from
       end
     end
+    # NOTE list_optimized has 2 nested iterations 
+    # second iteration is shortened due to optimization
+    # resulting in reduced complexity O(n*sqrt(n))
     def list_optimized total, candidate: 2
       primes = []
       while primes.count < total
@@ -41,6 +45,8 @@ class PrimeMulti
       end
       primes
     end
+    # NOTE list_fibers is algorithmically the same as list_optimized 
+    # but it works a little faster due to multithreading efficiency of Fibers
     def list_fibers total, candidate: 2
       raise ArgumentError unless total.is_a? Integer
       prime = Fiber.new do
@@ -61,6 +67,12 @@ class PrimeMulti
       table.print
     end
     private
+      # NOTE http://stackoverflow.com/questions/5811151/why-do-we-check-upto-the-square-root-of-a-prime-number-to-determine-if-it-is-pri
+      # If a number n is not a prime, it can be factored into two factors a and b:
+      # n = a*b
+      # If both a and b were greater than the square root of n, a*b would be greater than n. 
+      # So at least one of those factors must be less or equal to the square root of n, 
+      # and to check if n is prime, we only need to test for factors less than or equal to the square root.
       def is_prime? candidate
         return false if candidate <= 1
         2.upto Math.sqrt(candidate).to_i do |x|
@@ -68,6 +80,7 @@ class PrimeMulti
         end 
         true
       end 
+      # NOTE multiplication table is O(n^2) 
       def multi list
         raise ArgumentError unless list.respond_to? :each
         list.map do |first|
